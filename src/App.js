47ea24ac05118295d452eff66ae90f7f46e345d5
Ryan David Forsyth <forsyth.r@gmail.com>
2019-03-27 22:29:35 -0400

    Implement Clarifai package and test with Color model

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import Particles from 'react-particles-js';
 
+//Node packages
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai'; 
+
+//Components
 import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo';
 import ImageLinkForm from './components/imageLinkForm/imageLinkForm';
 import Rank from './components/rank/rank';
+import FaceRecognition from './components/faceRecognition/faceRecognition';
+
+const app = new Clarifai.App( {
+  apiKey: '52d56e21bc684677955e8b09444dd647'
+});
 
 const particlesOptions = {
   particles: {
@@ -20,6 +29,36 @@ const particlesOptions = {
 }
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+    }
+  }
+
+  //event listeners receive events!
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onSubmit = () => {
+
+    this.setState({imageUrl: this.state.input});
+
+    app.models.predict(
+      Clarifai.COLOR_MODEL, 
+      this.state.input).then(
+    function(response) {
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+  }
+
   render() {
     return (
       <div className="App">
@@ -30,10 +69,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/*
-          <FaceRecognition />
-        */}
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onSubmit={this.onSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
 
       </div>
     );
@@ -41,3 +81,6 @@ class App extends Component {
 }
 
 export default App;
+
+/*updated URL: "https://samples.clarifai.com/metro-north.jpg"  
+API name: Clarifai.FACE_DETECT_MODEL*/
--- src/components/imageLinkForm/imageLinkForm.js
+++ src/components/imageLinkForm/imageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './imageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ( { onInputChange , onSubmit } ) => {
 
 	return (
 
@@ -12,8 +12,12 @@ const ImageLinkForm = () => {
 			</p>
 			<div className='center'>
 				<div className='form center pa4 br3 shadow-5'>
-					<input className='f4 pa2 w-70 center' type='text' />
-					<button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+					<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
+					<button 
+						className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+						onClick={onSubmit}
+						>Detect
+					</button>
 				</div>
 			</div>
 
