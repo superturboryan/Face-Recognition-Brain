64b546dde2ee932c79decbde061a7e7bd09b2121
Ryan David Forsyth <forsyth.r@gmail.com>
2019-03-27 20:54:15 -0400

    Add react-particles-js npm

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
 
 import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo';
 import ImageLinkForm from './components/imageLinkForm/imageLinkForm';
 import Rank from './components/rank/rank';
 
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+
+        <Particles className='particles' 
+              params={particlesOptions}
+        />
         <Navigation />
         <Logo />
         <Rank />
@@ -18,6 +34,7 @@ class App extends Component {
         {/*
           <FaceRecognition />
         */}
+
       </div>
     );
   }
